f3d16a1ec7fda086b6c0c8401102d6d046208496
Hyblocker <hyblocker@protonmail.com>
2022-08-17 14:55:18 +0200

    Add GitHub links to the bottom of pages automatically

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,11 @@ export const SITE = {
     root: "https://k2vr.tech",
     embedColor: "#E33388",
     docsRoot: "/en/", // TODO: Localisation support
+
+    // Edit on GitHub props
+    // To make this work on GitLab: https://gitlab.com/someuser/somerepo/-
+    gitRepo:        "https://github.com/KinectToVR/k2vr-docs",
+    gitBranch:      "master"
 };
 
 export const SOCIALS = {
